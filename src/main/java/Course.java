372d3ecc4d6d1d9f06724d3f8e4112a5baf3e232
HARMONSC21 <harmonsc21@gcc.edu>
2025-02-24 11:17:19 -0500

    removed testing from Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -65,27 +65,4 @@ public class Course {
         }
         return false;
     }
-
-    // testing isOverlap
-    public static void main(String[] args) {
-        Course c1 = new Course(0);
-        c1.times = new ArrayList<>();
-        c1.times.add(new MeetingTime(new Time(1), new Time(10), "Monday"));
-        Course c2 = new Course(1);
-        c2.times = new ArrayList<>();
-        c2.times.add(new MeetingTime(new Time(1), new Time(10), "Monday"));
-        System.out.println(c1.isOverlap(c2));
-        Course c3 = new Course(2);
-        c3.times = new ArrayList<>();
-        c3.times.add(new MeetingTime(new Time(5), new Time(10), "Monday"));
-        System.out.println(c1.isOverlap(c3));
-        Course c4 = new Course(3);
-        c4.times = new ArrayList<>();
-        c4.times.add(new MeetingTime(new Time(10), new Time(20), "Monday"));
-        System.out.println(c1.isOverlap(c4));
-        Course c5 = new Course(4);
-        c5.times = new ArrayList<>();
-        c5.times.add(new MeetingTime(new Time(1), new Time(10), "Tuesday"));
-        System.out.println(c1.isOverlap(c5));
-    }
 }
