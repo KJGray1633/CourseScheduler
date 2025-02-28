c9928d69d9d7b44d0bda0fb6b923c1865a240ac5
Graybill <graybillkb20@gcc.edu>
2025-02-27 23:30:15 -0500

    Created and implemented day enum

--- src/main/java/MeetingTime.java
+++ src/main/java/MeetingTime.java
@@ -1,9 +1,19 @@
 import java.sql.Time;
 
+enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
 public class MeetingTime {
     private final Time startTime;
     private final Time endTime;
-    private final String day;
+    private final Day day;
 
     /**
      *
@@ -14,9 +24,33 @@ public class MeetingTime {
     public MeetingTime(Time startTime, Time endTime, String day) {
         this.startTime = startTime;
         this.endTime = endTime;
-        this.day = day;
+        String dayCopy = day.strip().toLowerCase();
+        switch (dayCopy) {
+            case "sunday":
+                this.day = Day.SUNDAY;
+                break;
+            case "monday":
+                this.day = Day.MONDAY;
+                break;
+            case "tuesday":
+                this.day = Day.TUESDAY;
+                break;
+            case "wednesday":
+                this.day = Day.WEDNESDAY;
+                break;
+            case "thursday":
+                this.day = Day.THURSDAY;
+                break;
+            case "friday":
+                this.day = Day.FRIDAY;
+                break;
+            case "saturday":
+                this.day = Day.SATURDAY;
+                break;
+            default: throw new IllegalArgumentException(dayCopy + " is not a valid day");
+        }
     }
-    //
+
     @Override
     public String toString() {
         return "MeetingTime{" +
@@ -34,7 +68,7 @@ public class MeetingTime {
         return endTime;
     }
 
-    public String getDay() {
+    public Day getDay() {
         return day;
     }
 }
