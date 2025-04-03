d61ef5350464c887809a37135b881d1667952597
Ruby Greathouse <greathouserc20@gcc.edu>
2025-04-03 17:42:26 -0400

    db test

--- src/main/java/DBTest.java
+++ src/main/java/DBTest.java
@@ -2,12 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.*;
+import java.sql.Statement;
 
 
 public class DBTest {
     public static void main(String[] args) {
         Connection conn = null;
-
+        Statement stmt = null;
         try {
             Properties info = new Properties();
 
@@ -22,9 +23,20 @@ public class DBTest {
 
             System.out.println("Connection successful!");
 
+            stmt  = conn.createStatement();
+            String creatUsersTable = "CREATE TABLE IF NOT EXISTS users ("
+                    + "id INT AUTO_INCREMENT PRIMARY KEY, "
+                    + "uid VARCHAR(10) NOT NULL, "
+                    + "cid VARCHAR(20), ";
+            stmt.execute(creatUsersTable);
+            System.out.println("'users' table created successfully!");
             conn.close();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+
+
+
+
     }
 }
