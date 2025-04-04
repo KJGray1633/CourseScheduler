5b717bd2a156e6487f67240d92bf62b089bf0655
Ruby Greathouse <greathouserc20@gcc.edu>
2025-04-04 15:45:13 -0400

    created courses table

--- src/main/java/DBTest.java
+++ src/main/java/DBTest.java
@@ -27,12 +27,37 @@ public class DBTest {
 
             // Create the new table
             String createUsersTable = "CREATE TABLE IF NOT EXISTS users ("
-                    + "uid VARCHAR(10) NOT NULL, "
-                    + "cid VARCHAR(20), "
+                    + "uid INT NOT NULL, "
+                    + "cid INT, "
+                    + "username VARCHAR(50), "
+                    + "password VARCHAR(50), "
+                    + "major VARCHAR(50), "
+                    + "year VARCHAR(50), "
                     + "PRIMARY KEY (uid, cid))";
             pstmt = conn.prepareStatement(createUsersTable);
             pstmt.executeUpdate();
             System.out.println("'users' table created successfully!");
+
+            // Drop the courses table if it exists
+            String dropCourseTable = "DROP TABLE IF EXISTS courses";
+            pstmt = conn.prepareStatement(dropCourseTable);
+            pstmt.executeUpdate();
+            System.out.println("Table 'courses' dropped successfully!");
+
+            // Create the courses table
+            String createCoursesTable = "CREATE TABLE IF NOT EXISTS courses ("
+                    + "cid INT NOT NULL, "
+                    + "name VARCHAR(50), "
+                    + "courseCode VARCHAR(10), "
+                    + "times VARCHAR(50), "
+                    + "referenceNum INT, "
+                    + "PRIMARY KEY (cid))";
+
+            pstmt = conn.prepareStatement(createCoursesTable);
+            pstmt.executeUpdate();
+            System.out.println("'courses' table created successfully!");
+
+
 
             conn.close();
         } catch (SQLException ex) {
